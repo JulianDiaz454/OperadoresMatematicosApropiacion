c9c68f49bb6d5f29d08eea9d8844087d899060fb
JulianDiaz <torresanibal336@gmail.com>
2025-10-06 17:41:59 -0500

    Se creo un nuevo archivo JS, se dio solución a el ejercicio número 4 de la guia

---
+++ ContadorVisitas.js
@@ -0,0 +1,14 @@
+// Declaramos el contador
+let contador = 100; // Inicializamos en 100
+
+// Sumamos 25 visitantes al contador
+contador += 25;
+
+// Le restamos 10 al contador
+contador -= 10;
+
+// Multiplicamos el contador por 2
+contador = contador * 2;
+
+// Mostramos el resultado 
+console.log("CONTADOR FINAL: " + contador );
