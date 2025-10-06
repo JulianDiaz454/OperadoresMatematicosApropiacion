a5d5ca8f760e9ae12d63ddbac8595496411b7ace
JulianDiaz <torresanibal336@gmail.com>
2025-10-06 17:59:06 -0500

    Se realizan crea un nuevo archivo JS, este da solcuón al ultimo ejercicio de la guia

---
+++ OperacionMatematica.js
@@ -0,0 +1,13 @@
+// Creamos la operación
+let operacion = (20+10)/5+3*2-4*(20+10) / 5+3*2 -4*(20+10)/5+3*2-4;
+
+// Primero se realizan las operaciones dentro de los parentesis
+
+// Se realizan las divisiones
+
+// Se realizan las sumas
+
+// Se realizan las restas
+
+// Mostramos el resultado
+console.log("RESULTADO: " + operacion);
